4b092a121daf1e63356bb276adfa8954770bca88
fu shuai <128142181+fushuais@users.noreply.github.com>
2026-02-23 15:04:17 +0900

    Initial Commit

---
+++ travel/travelApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelApp.swift
+//  travel
+//
+//  Created by fushuai on 2026/2/23.
+//
+
+import SwiftUI
+
+@main
+struct travelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ travelTests/travelTests.swift
@@ -0,0 +1,17 @@
+//
+//  travelTests.swift
+//  travelTests
+//
+//  Created by fushuai on 2026/2/23.
+//
+
+import Testing
+@testable import travel
+
+struct travelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
